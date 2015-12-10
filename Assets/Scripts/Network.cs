7a0c6e4f022d8545ce01c3388dc0c7cbff2f9597
Matthew Gerig <mattgerig@gmail.com>
2015-12-10 11:20:42 +0000

    Pushing project to the git repo

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,417 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+using System;
+using System.Diagnostics;
+
+public class Network : MonoBehaviour
+{
+    ArrayList updateMessage = new ArrayList(10000);
+    ConnectionConfig config;
+    int myReliableChannelId;
+    int myUnreliableChannelId;
+    HostTopology topology;
+    int hostId;
+    int socketId;
+    int socketPort = 8888;
+    int connectionId;
+    byte error;
+    bool sendPosition = false;
+    int mainPlayer;
+    int timer = 5;
+
+    bool isHost = false;
+    int numberPlayers = 0;
+
+    Stopwatch messageStopWatch;
+    float messageTime;
+
+    bool deadReckoning = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        messageStopWatch = new Stopwatch();
+
+        GameObject temp;
+        temp = GameObject.Find("YouWon1");
+        temp.GetComponent<Image>().enabled = false;
+        temp = GameObject.Find("YouWon2");
+        temp.GetComponent<Image>().enabled = false;
+        temp = GameObject.Find("YouLost1");
+        temp.GetComponent<Image>().enabled = false;
+        temp = GameObject.Find("YouLost2");
+        temp.GetComponent<Image>().enabled = false;
+        temp = GameObject.Find("IsOn1");
+        temp.GetComponent<Image>().enabled = false;
+        temp = GameObject.Find("IsOn2");
+        temp.GetComponent<Image>().enabled = false;
+
+        NetworkTransport.Init();
+        config = new ConnectionConfig();
+        myReliableChannelId = config.AddChannel(QosType.Reliable);
+        int maxConnections = 100;
+        topology = new HostTopology(config, maxConnections);
+        socketId = NetworkTransport.AddHost(topology, socketPort);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player;
+
+        int recHostId;
+        int recConnectionId;
+        int recChannelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        byte error;
+        NetworkEventType recNetworkEvent = NetworkTransport.Receive(out recHostId, out recConnectionId, out recChannelId, recBuffer, bufferSize, out dataSize, out error);
+
+        switch (recNetworkEvent)
+        {
+            case NetworkEventType.Nothing:
+                break;
+            case NetworkEventType.ConnectEvent:
+                UnityEngine.Debug.Log("Connection Occurred");
+
+                if (isHost)
+                {
+                    numberPlayers++;
+
+                    UnityEngine.Debug.Log("Number of players is: " + numberPlayers);
+
+                    if (numberPlayers == 3)
+                    {
+                        System.Random random = new System.Random();
+                        int randomNumber = random.Next(0, 2);
+
+                        if (randomNumber == 0)
+                        {
+                            player = GameObject.Find("Player1");
+                            player.GetComponent<Player>().IsOn(true);
+                            GameObject tempy;
+                            tempy = GameObject.Find("IsOn1");
+                            tempy.GetComponent<Image>().enabled = true;
+                        }
+                        else
+                        {
+                            GameObject tempy;
+                            tempy = GameObject.Find("IsOn2");
+                            tempy.GetComponent<Image>().enabled = true;
+                        }
+
+                        updateMessage.Clear();
+                        updateMessage.Add((byte)'S');
+                        updateMessage.AddRange(System.BitConverter.GetBytes(randomNumber));
+
+                        byte[] messageToSend = new byte[updateMessage.Count];
+                        messageToSend = (byte[])updateMessage.ToArray(typeof(byte));
+
+                        byte[] buffer = new byte[1024];
+                        Stream streamy = new MemoryStream(buffer);
+                        BinaryFormatter formattery = new BinaryFormatter();
+                        formattery.Serialize(streamy, messageToSend);
+
+                        NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+
+                        GameObject temp;
+                        temp = GameObject.Find("background");
+                        temp.transform.position = new Vector2(-1000000, 0);
+                        temp = GameObject.Find("JoinButton");
+                        temp.transform.position = new Vector2(-1000000, 0);
+                        temp = GameObject.Find("HostButton");
+                        temp.transform.position = new Vector2(-1000000, 0);
+                        temp = GameObject.Find("ExitButton");
+                        temp.transform.position = new Vector2(-1000000, 0);
+
+                        sendPosition = true;
+                    }
+                }
+                break;
+            case NetworkEventType.DataEvent:
+                Stream stream = new MemoryStream(recBuffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                byte[] message = formatter.Deserialize(stream) as byte[];
+
+                char messageType = Convert.ToChar(message[0]);
+
+                if (messageType == 'S')
+                {
+                    GameObject temp;
+                    temp = GameObject.Find("background");
+                    temp.transform.position = new Vector2(-1000000, 0);
+                    temp = GameObject.Find("JoinButton");
+                    temp.transform.position = new Vector2(-1000000, 0);
+                    temp = GameObject.Find("HostButton");
+                    temp.transform.position = new Vector2(-1000000, 0);
+                    temp = GameObject.Find("ExitButton");
+                    temp.transform.position = new Vector2(-1000000, 0);
+                    sendPosition = true;
+
+                    if (System.BitConverter.ToSingle(message, 1) != 0)
+                    {
+                        player = GameObject.Find("Player1");
+                        player.GetComponent<Player>().IsOn(true);
+                        GameObject tempy;
+                        tempy = GameObject.Find("IsOn2");
+                        tempy.GetComponent<Image>().enabled = true;
+                    }
+                    else
+                    {
+                        GameObject tempy;
+                        tempy = GameObject.Find("IsOn1");
+                        tempy.GetComponent<Image>().enabled = true;
+                    }
+
+                    messageStopWatch.Start();
+                }
+
+                //recieved update message from other player
+                if (messageType == 'U')
+                {
+                    float posX = System.BitConverter.ToSingle(message, 1);
+                    float posY = System.BitConverter.ToSingle(message, 5);
+                    float velX = System.BitConverter.ToSingle(message, 9);
+                    float velY = System.BitConverter.ToSingle(message, 13);
+                    float rot = System.BitConverter.ToSingle(message, 17);
+                    UnityEngine.Debug.Log("Player 2 is at (" + posX + ", " + posY + ") traveling (" + velX + ", " + velY + ")");
+
+                    //interpolation methods
+                    messageTime = (float)messageStopWatch.ElapsedMilliseconds;
+                    var player2 = GameObject.Find("SecondPlayer");
+                    Player temp = (Player)player2.GetComponent(typeof(Player));
+                    if (deadReckoning)
+                        temp.DeadReckoningUpdate(new Vector2(posX, posY), new Vector2(velX, velY));
+                    else
+                        temp.StartInterpolation(messageTime, new Vector2(posX, posY));
+
+                    messageStopWatch.Reset();
+                    messageStopWatch.Start();
+
+                    //moving the player OLD
+                    /*var player2 = GameObject.Find("SecondPlayer");
+                    player2.transform.position = new Vector2(posX, posY);
+                    player2.transform.rotation = Quaternion.Euler(0, 0, rot);
+                    player2.GetComponent<Rigidbody2D>().MoveRotation(rot);*/
+
+                }
+
+                //recieved push message from other player
+                if (messageType == 'P')
+                {
+                    float posX = System.BitConverter.ToSingle(message, 1);
+                    float posY = System.BitConverter.ToSingle(message, 5);
+                    Vector2 tempVec = new Vector2(posX, posY);
+
+                    messageTime = (float)messageStopWatch.ElapsedMilliseconds;
+                    var player1 = GameObject.Find("Player1");
+                    Player temp = (Player)player1.GetComponent(typeof(Player));
+                    temp.HandlePush(tempVec);
+
+                    var player2 = GameObject.Find("SecondPlayer");
+                    player2.GetComponent<ParticleSystem>().Play();
+
+                }
+
+                if (messageType == 'E')
+                {
+                    GameObject temp;
+                    temp = GameObject.Find("Player1");
+
+                    if (temp.GetComponent<Player>().IsPlayerOn())
+                    {
+                        temp = GameObject.Find("YouWon2");
+                        temp.GetComponent<Image>().enabled = true;
+                    }
+                    else
+                    {
+                        temp = GameObject.Find("YouLost2");
+                        temp.GetComponent<Image>().enabled = true;
+                    }
+
+                    GameObject tempy;
+                    tempy = GameObject.Find("IsOn1");
+                    tempy.GetComponent<Image>().enabled = false;
+                    tempy = GameObject.Find("IsOn2");
+                    tempy.GetComponent<Image>().enabled = false;
+                }
+
+                break;
+            case NetworkEventType.DisconnectEvent:
+                UnityEngine.Debug.Log("remote client event disconnected");
+                break;
+        }
+
+        if (!deadReckoning)
+        {
+            timer--;
+
+            if (sendPosition)
+            {
+                if (timer <= 0)
+                {
+                    SendPlayerUpdate();
+                    timer = 3;
+                }
+            }
+        }
+    }
+
+    public void createHost()
+    {
+        UnityEngine.Debug.Log("Socket Open. Socket ID = " + socketId);
+        connectionId = NetworkTransport.Connect(socketId, "149.153.102.61", socketPort, 0, out error);
+
+        GameObject myButton;
+
+        myButton = GameObject.Find("HostButton");
+        myButton.GetComponent<Button>().interactable = false;
+        myButton = GameObject.Find("JoinButton");
+        myButton.GetComponent<Button>().interactable = false;
+
+        mainPlayer = 1;
+
+        numberPlayers++;
+
+        GameObject player;
+        player = GameObject.Find("Player1");
+        player.GetComponent<Player>().SetPlayer1(true);
+
+        player = GameObject.Find("Player2");
+        player.GetComponent<Player>().SetPlayer2(true);
+        player.name = "SecondPlayer";
+
+        isHost = true;
+    }
+
+    public void connectToHost()
+    {
+        connectionId = NetworkTransport.Connect(socketId, "149.153.102.56", socketPort, 0, out error);
+        UnityEngine.Debug.Log("Connected to server. ConnectionId: " + connectionId);
+
+        GameObject myButton;
+
+        myButton = GameObject.Find("HostButton");
+        myButton.GetComponent<Button>().interactable = false;
+        myButton = GameObject.Find("JoinButton");
+        myButton.GetComponent<Button>().interactable = false;
+
+        mainPlayer = 2;
+
+        GameObject player;
+
+        player = GameObject.Find("Player1");
+        player.GetComponent<Player>().SetPlayer2(true);
+        player.name = "SecondPlayer";
+
+        player = GameObject.Find("Player2");
+        player.GetComponent<Player>().SetPlayer1(true);
+        player.name = "Player1";
+
+        isHost = false;
+
+        //myButton = GameObject.Find("MessageButton");
+        //myButton.GetComponent<Button>().interactable = true;
+    }
+
+    public void SendPlayerUpdate()
+    {
+        GameObject player;
+        player = GameObject.Find("Player1");
+        float posX = player.transform.position.x;
+        float posY = player.transform.position.y;
+        Vector2 velocity = player.GetComponent<Rigidbody2D>().velocity;
+
+        updateMessage.Clear();
+        updateMessage.Add((byte)'U');
+        updateMessage.AddRange(System.BitConverter.GetBytes(posX));
+        updateMessage.AddRange(System.BitConverter.GetBytes(posY));
+        updateMessage.AddRange(System.BitConverter.GetBytes(velocity.x));
+        updateMessage.AddRange(System.BitConverter.GetBytes(velocity.y));
+        updateMessage.AddRange(System.BitConverter.GetBytes(velocity.y));
+        Player other = (Player)player.GetComponent(typeof(Player));
+        updateMessage.AddRange(System.BitConverter.GetBytes(other.GetAngle()));
+
+        byte[] messageToSend = new byte[updateMessage.Count];
+        messageToSend = (byte[])updateMessage.ToArray(typeof(byte));
+
+        byte error;
+        byte[] buffer = new byte[1024];
+        Stream stream = new MemoryStream(buffer);
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, messageToSend);
+
+        int bufferSize = 1024;
+
+        NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+    }
+
+    private float RadianToDegree(float angle1)
+    {
+        return (float)(angle1 * (180.0 / Math.PI));
+    }
+
+    public void SendEndMessage()
+    {
+        if (isHost)
+        {
+            updateMessage.Clear();
+            updateMessage.Add((byte)'E');
+
+            byte[] messageToSend = new byte[updateMessage.Count];
+            messageToSend = (byte[])updateMessage.ToArray(typeof(byte));
+
+            byte[] buffer = new byte[1024];
+            int bufferSize = 1024;
+            Stream streamy = new MemoryStream(buffer);
+            BinaryFormatter formattery = new BinaryFormatter();
+            formattery.Serialize(streamy, messageToSend);
+
+            NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+
+            GameObject temp;
+            temp = GameObject.Find("Player1");
+
+            if (temp.GetComponent<Player>().IsPlayerOn())
+            {
+                temp = GameObject.Find("YouWon1");
+                temp.GetComponent<Image>().enabled = true;
+            }
+            else
+            {
+                temp = GameObject.Find("YouLost1");
+                temp.GetComponent<Image>().enabled = true;
+            }
+
+            GameObject tempy;
+            tempy = GameObject.Find("IsOn1");
+            tempy.GetComponent<Image>().enabled = false;
+            tempy = GameObject.Find("IsOn2");
+            tempy.GetComponent<Image>().enabled = false;
+        }
+    }
+
+    public void SendPush(Vector2 playerPos)
+    {
+        updateMessage.Clear();
+        updateMessage.Add((byte)'P');
+        updateMessage.AddRange(System.BitConverter.GetBytes(playerPos.x));
+        updateMessage.AddRange(System.BitConverter.GetBytes(playerPos.y));
+
+        byte[] messageToSend = new byte[updateMessage.Count];
+        messageToSend = (byte[])updateMessage.ToArray(typeof(byte));
+
+        byte[] buffer = new byte[1024];
+        int bufferSize = 1024;
+        Stream streamy = new MemoryStream(buffer);
+        BinaryFormatter formattery = new BinaryFormatter();
+        formattery.Serialize(streamy, messageToSend);
+
+        NetworkTransport.Send(socketId, connectionId, myReliableChannelId, buffer, bufferSize, out error);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,232 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Player : MonoBehaviour
+{
+
+    public bool player1;
+    public float moveSpeed = 4f;
+    public float maxSpeed = 5f;
+    float angle;
+
+    private Rigidbody2D rb2d;
+
+    Vector2 mousePosition;
+
+    public int boostTimer = 60;
+    public int boostFire = 60;
+    public float boostPower = 100;
+
+    bool isOn = false;
+    bool gameOn = false;
+
+    bool interpolatingMove = false;
+    float interpolationTime;
+    Stopwatch interpolateWatch;
+    Vector2 targetPosition;
+    Vector2 positionAtUpdate;
+    Vector2 distance;
+
+    Vector2 ghostPos;
+    Vector2 ghostVel;
+
+    //starts on
+    bool extrapolation = true;
+
+    bool deadReckoning = true;
+
+    float pushTimer = 120;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        interpolateWatch = new Stopwatch();
+        ghostPos = new Vector2(transform.position.x, transform.position.y);
+        ghostVel = rb2d.velocity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOn)
+        {
+            if (player1)
+            {
+                mousePosition = Input.mousePosition;
+                Vector2 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+                mousePosition.x = mousePosition.x - objectPos.x;
+                mousePosition.y = mousePosition.y - objectPos.y;
+                angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+                rb2d.MoveRotation(angle);
+
+                boostTimer++;
+                pushTimer++;
+
+                float dist = Vector2.Distance(transform.position, ghostPos);
+
+                //whern the distance between the two is larger than specified amount, then send update!
+                if (dist > 1 && deadReckoning)
+                {
+                    //updating ghost position and velocity
+                    ghostPos = transform.position;
+                    ghostVel = rb2d.velocity;
+                    UnityEngine.Debug.Log("Distance exceeded");
+
+                    GameObject net;
+                    net = GameObject.Find("Net");
+                    net.GetComponent<Network>().SendPlayerUpdate();
+                }
+
+                ghostPos.x += ghostVel.x;
+                ghostPos.y += ghostVel.y;
+
+                if (boostTimer > 120)
+                {
+                    maxSpeed = 5f;
+                }
+
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+                {
+
+                    if (Input.GetKey(KeyCode.W))
+                    {
+                        rb2d.AddForce(transform.right * moveSpeed);
+                    }
+                    if (Input.GetKey(KeyCode.S))
+                    {
+                        rb2d.AddForce(-transform.right * moveSpeed);
+                    }
+                }
+                else
+                {
+                    rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y) * 0.9f;
+                }
+
+                if (rb2d.velocity.magnitude > maxSpeed)
+                {
+                    rb2d.velocity = rb2d.velocity.normalized * maxSpeed;
+                }
+
+                if (Input.GetKey(KeyCode.Space) && boostTimer > 120)
+                {
+                    boostTimer = 0;
+                    maxSpeed = 10f;
+                    rb2d.AddForce(transform.right * boostPower);
+                }
+
+                if (Input.GetKey(KeyCode.E) && pushTimer > 120)
+                {
+                    pushTimer = 0;
+                    GameObject net;
+                    net = GameObject.Find("Net");
+                    net.GetComponent<Network>().SendPush(new Vector2(transform.position.x, transform.position.y));
+                    gameObject.GetComponent<ParticleSystem>().Play();
+                }
+            } 
+            else
+            {
+                InterpolateToPosition();
+            }
+        }
+    }
+
+    public void SetPlayer1(bool isPlayer1)
+    {
+        player1 = isPlayer1;
+        gameOn = true;
+    }
+
+    public void SetPlayer2(bool isPlayer1)
+    {
+        gameOn = true;
+    }
+
+    public float GetAngle()
+    {
+        return angle;
+    }
+
+    public void IsOn(bool yes)
+    {
+        isOn = yes;
+    }
+
+    public bool IsPlayerOn()
+    {
+        return isOn;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player1" || col.gameObject.name == "SecondPlayer")
+        {
+            gameOn = false;
+            GameObject net;
+            net = GameObject.Find("Net");
+            net.GetComponent<Network>().SendEndMessage();
+        }
+    }
+
+    public void StartInterpolation(float time, Vector2 pos)
+    {
+        positionAtUpdate = transform.position;
+
+        interpolatingMove = true;
+        interpolationTime = time;
+        //starting watch for interpolating - time since update
+        interpolateWatch.Reset();
+        interpolateWatch.Start();
+
+        //position trying to get to
+        distance = pos - positionAtUpdate;
+    }
+
+    public void InterpolateToPosition()
+    {
+        //Stops the NaN error!
+        if (interpolatingMove == true)
+        {
+            //interpolation to next position
+            if (interpolationTime > 0)
+            {
+                //using distance and the time taken since last update, interpolate towards the position that was sent last.
+                Vector2 tempToAddToPos = new Vector2(distance.x * (interpolateWatch.ElapsedMilliseconds / interpolationTime), distance.y * (interpolateWatch.ElapsedMilliseconds / interpolationTime));
+                transform.position = new Vector2(positionAtUpdate.x + tempToAddToPos.x, positionAtUpdate.y + tempToAddToPos.y);
+
+                //dead reckoning. The player will continue in the direction and at the speed from the last update till a new update is received
+                if (interpolateWatch.ElapsedMilliseconds > interpolationTime && extrapolation)
+                {
+                    interpolateWatch.Reset();
+                    interpolateWatch.Start();
+                    positionAtUpdate = transform.position;
+                }
+            }
+        }
+    }
+
+    public void HandlePush(Vector2 pos)
+    {
+        //getting distane between two players
+        Vector2 tempPlayerPos = new Vector2(transform.position.x, transform.position.y);
+        float distanceBetween = Vector2.Distance(tempPlayerPos, pos);
+
+        //how far away the push will work from
+        if (distanceBetween < 2)
+        {
+            Vector2 direction = (tempPlayerPos - pos).normalized;
+            float force = 1000;
+            rb2d.AddForce(direction * force);
+        }
+    }
+
+    public void DeadReckoningUpdate(Vector2 pos, Vector2 vel)
+    {
+        //updating the player2 position and velocity when there is a difference of a certain amount
+        transform.position = pos;
+        rb2d.velocity = vel;
+    }
+}
